33f07b634fa3bef71da03b653ee8155746268c52
Raefe <raefe.nj@gmail.com>
2018-05-01 10:05:39 +0100

    added is_divisible_by method

---
+++ fizzbuzz.py
@@ -0,0 +1,4 @@
+class fizzbuzz:
+
+  def is_divisible_by(number, divisor):
+    return number % divisor == 0
---
+++ test_fizzbuzz.py
@@ -0,0 +1,6 @@
+from fizzbuzz import fizzbuzz
+
+
+def test_is_divisible_by():
+    result = fizzbuzz.is_divisible_by(15, 5)
+    assert result == True
