8342275ef24c8a0a1d5830c6e54b6916dd0e5ed0
Raefe <raefe.nj@gmail.com>
2018-05-01 10:39:06 +0100

    refactored

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,6 @@ def test_is_divisible_by():
     result = fizzbuzz.is_divisible_by(15, 5)
     assert result == True
 
-def test_is_divisible_by_by_15():
+def test_is_divisible_by_15():
     result = fizzbuzz.is_divisible_by_15(60)
     assert result == True
