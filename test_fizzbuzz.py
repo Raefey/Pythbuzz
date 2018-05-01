347e6370757fc3b3523d589bf44536c3681aad3e
Raefe <raefe.nj@gmail.com>
2018-05-01 10:52:59 +0100

    added false return valuesfor some tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,14 +1,20 @@
 from fizzbuzz import fizzbuzz
 
 
-def test_is_divisible_by():
-    result = fizzbuzz.is_divisible_by(15, 5)
-    assert result == True
+def test_is_divisible_by_true():
+    assert fizzbuzz.is_divisible_by(15, 5) == True
 
-def test_is_divisible_by_15():
-    result = fizzbuzz.is_divisible_by_15(60)
-    assert result == True
+def test_is_divisible_by_false():
+    assert fizzbuzz.is_divisible_by(107, 5) == False
 
-def test_is_divisible_by_5():
-    result = fizzbuzz.is_divisible_by_5(100)
-    assert result == True
+def test_is_divisible_by_15_true():
+    assert fizzbuzz.is_divisible_by_15(60) == True
+
+def test_is_divisible_by_15_false():
+    assert fizzbuzz.is_divisible_by_15(72) == False
+
+def test_is_divisible_by_5_true():
+    assert fizzbuzz.is_divisible_by_5(100) == True
+
+def test_is_divisible_by_5_false():
+    assert fizzbuzz.is_divisible_by_5(73) == False
